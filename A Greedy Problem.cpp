f04e93ceb92b1eb7ae38bc45a85ebb7eb1fba1f2
Fiona Cheng <83597131+Indecisive613@users.noreply.github.com>
2022-08-04 13:41:44 -0400

    Create A Greedy Problem.cpp

---
+++ A Greedy Problem.cpp
@@ -0,0 +1,30 @@
+//https://dmoj.ca/problem/greedy
+#include<bits/stdc++.h> 
+using namespace std; 
+int main() {
+    int N, a, b;//num, goal
+    long long int M;
+    long long int counter = 0;
+    long long int money = 0;
+    vector < pair<int,int> > factory;
+    cin>> N>>M;
+    
+    for (int i = 0; i < N; i++) {
+        cin >> a >> b;
+        factory.push_back(make_pair(a,b));
+    }
+    
+    sort(factory.begin(), factory.end()); 
+
+    for (int i = 0; counter < M; i++) {
+        if (counter + factory[i].second <= M) {
+            counter = counter+factory[i].second;
+            money = money + factory[i].first*factory[i].second;
+        }
+        else{
+            money = money + (M-counter)*factory[i].first;
+            counter = M;
+        }
+    }
+    cout << money << endl;
+}
