f3f2e6aff2981adb327437423e45d56ef29b4680
Fiona Cheng <83597131+Indecisive613@users.noreply.github.com>
2022-08-04 14:00:37 -0400

    Create Zero That Out.cpp

---
+++ Zero That Out.cpp
@@ -0,0 +1,24 @@
+//https://dmoj.ca/problem/ccc15s1
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int K, boss, sum = 0;
+    vector<int> num;
+    cin >> K;
+   
+    for (int i = 0; i < K; i++) {
+        cin >> boss;
+        if (boss == 0) {
+            num.resize(num.size()-1);
+        }
+        else {
+            num.push_back(boss);
+        }
+    }
+    for (int i = 0; i < num.size(); i++) {
+       sum = sum + num[i];
+    }
+    cout << sum;
+
+    return 0;
+}
