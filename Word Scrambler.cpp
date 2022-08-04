bfc201f4fcac33a2cde09504d61ef21c20fb219e
Fiona Cheng <83597131+Indecisive613@users.noreply.github.com>
2022-08-04 14:05:41 -0400

    Create Word Scrambler.cpp

---
+++ Word Scrambler.cpp
@@ -0,0 +1,16 @@
+//https://dmoj.ca/problem/dwite09c1p2
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string s[5];
+    for (int i = 0; i < 5; i++) {
+        getline(cin, s[i]);
+        sort(s[i].begin(), s[i].end());
+    }
+    for (int i = 0; i < 5; i++) {
+        do {
+            cout << s[i] << '\n';
+        } while(std::next_permutation(s[i].begin(), s[i].end()));
+    }
+    return 0;
+}
