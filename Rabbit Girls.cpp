4bfb66c34e516429a776614c3d1baa72dd0ef8e4
Fiona Cheng <83597131+Indecisive613@users.noreply.github.com>
2022-08-04 14:03:11 -0400

    Create Rabbit Girls.cpp

---
+++ Rabbit Girls.cpp
@@ -0,0 +1,25 @@
+//https://dmoj.ca/problem/halloween14p1
+#include <iostream>
+using namespace std;
+int main() {
+    long int girls, group_size, dif;
+    cin >> girls >> group_size;
+    dif = girls%group_size;
+    
+    if (dif == 0) {
+        cout << dif;
+    }
+    else {
+        if (dif > group_size/2) {
+            cout << group_size-dif;
+        }
+        else {
+            if (group_size > girls) 
+                cout << group_size - girls;
+            else
+                cout << dif;
+        }
+    }
+
+    return 0;
+}
