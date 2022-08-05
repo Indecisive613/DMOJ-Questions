90ec3c65410d6371da0f81bf1cc5adfbaab6279c
Fiona Cheng <83597131+Indecisive613@users.noreply.github.com>
2022-08-05 10:08:37 -0400

    Create Herman.cpp

---
+++ Herman.cpp
@@ -0,0 +1,13 @@
+//https://dmoj.ca/problem/coci06c1p2
+//2020
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+    int R;
+    cin >> R;
+    
+    cout << fixed << setprecision(6) << M_PI*pow(R,2) << endl;
+    cout << fixed << setprecision(6) << M_PI*pow(R,2)/(M_PI/2.000000000000000) << endl;
+    return 0;
+}
