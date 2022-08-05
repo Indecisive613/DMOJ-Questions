528400f47a2b8f36dc5ede5e1514d288bc21f99c
Fiona Cheng <83597131+Indecisive613@users.noreply.github.com>
2022-08-05 09:55:25 -0400

    Update Number of Factors.cpp

--- Number of Factors.cpp
+++ Number of Factors.cpp
@@ -1,1 +1,29 @@
-
+//https://dmoj.ca/problem/dwite07c5p2
+//2019
+#include <iostream>
+using namespace std;
+int main() {
+    int primes[5] = {2, 3, 5, 7, 11};
+    int num, num0, counter;
+    int ans[5] = {0};
+    
+    for (int i = 0; i < 5; i++) {
+        counter = 0;
+        cin >> num0;
+        num = num0;
+        while (counter < 5) {
+            if (num%primes[counter] == 0 && num0!=primes[counter]) {
+                ans[i]++;
+                num = num/primes[counter];
+            }
+            else {
+                counter++;
+            }
+        }
+    }
+    
+    for (int i = 0; i < 5; i++) {
+        cout << ans[i] << endl;
+    }
+    return 0;
+}
